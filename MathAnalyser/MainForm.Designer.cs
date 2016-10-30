4573c117d672a221c8365a7ac892f621a070ab1c
MathMark <vadimmartsun@gmail.com>
2016-10-30 13:43:09 +0200

    Adjust height and width of the form

--- MathAnalyser/MainForm.Designer.cs
+++ MathAnalyser/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MathAnalyser
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
--- MathAnalyser/MainForm.cs
+++ MathAnalyser/MainForm.cs
@@ -10,11 +10,14 @@ using System.Windows.Forms;
 
 namespace MathAnalyser
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+            Rectangle screenSize = Screen.PrimaryScreen.Bounds;
+            this.Height = 2*screenSize.Size.Height/3;
+            this.Width= 2*screenSize.Size.Width / 3;
         }
     }
 }
